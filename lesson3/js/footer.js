b5b8de9811df5a1e508fb7d662e33a654cada767
bastianfr <fer17030@byui.edu>
2020-05-08 21:37:59 -0600

    week3

---
+++ lesson3/js/footer.js
@@ -0,0 +1,5 @@
+let year = new Date().getFullYear();
+        document.getElementById('date').innerHTML = year;
+
+        let update = document.lastModified;
+        document.getElementById('lastUpdate').innerHTML = update;
