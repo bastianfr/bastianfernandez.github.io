ec8182621554e78c6bd440e9cd1cc50ed323b724
bastianfr <fer17030@byui.edu>
2020-05-16 10:27:39 -0600

    week4

---
+++ lesson4/js/js.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
+}
+let daynames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+let months = ["January","February","March","April","May","June", "July","August","September","October","November","December"];
+
+let d = new Date();
+let dayName = daynames[d.getDay()];
+let monthName = months[d.getMonth()];
+let year = new Date().getFullYear();
+let fulldate = dayName + ", " + d.getDate() + " " + monthName + " " + year;
+
+document.getElementById('date').innerHTML = fulldate;
