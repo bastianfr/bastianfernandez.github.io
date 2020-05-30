85e704927c4ce43f9142b564aca76aa833efbed0
bastianfr <fer17030@byui.edu>
2020-05-30 16:31:59 -0600

    week6

--- lesson-6/js/windchill.js
+++ lesson-6/js/windchill.js
@@ -1,7 +1,10 @@
 
 var t = Number(document.getElementById("high").textContent);
 var s = Number(document.getElementById("windS").textContent);
-var f = (35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * (Math.pow(s,0.16))));
-var final = Math.round(f) + " °F";
-
+if (t <= 50 && s >= 3) { 
+    var f = (35.74 + (0.6215 * t) - (35.75 * Math.pow(s,0.16)) + (0.4275 * t * (Math.pow(s,0.16))));
+    var final = Math.round(f) + " °F";
+} else {
+    var final = 'N/A'
+}
 document.getElementById('windC').innerHTML = final;
